4038007bbd485ed9a426e0c87f57724d10da1b81
taufikdimas <143729231+taufikdimas@users.noreply.github.com>
2023-10-23 14:01:12 +0700

    fix:pertanyaan

--- Semester 1/Daspro/Pratikum/Jobsheet-9/ArrayRataNilai27.java
+++ Semester 1/Daspro/Pratikum/Jobsheet-9/ArrayRataNilai27.java
@@ -4,20 +4,30 @@ public class ArrayRataNilai27 {
     public static void main(String[] args) {
         Scanner sc27 = new Scanner(System.in);
 
-        int[] nilaiMhs = new int[10];
-        double total = 0;
-        double rata2;
+        System.out.print("Masukkan jumlah mahasiswa :");
+        int jmlMhs = sc27.nextInt();
+ 
+        int[] nilaiMhs = new int[jmlMhs];
+        double totalLulus = 0;
+        double totaltdklulus = 0;
+        double ratalulus, ratatdkLulus;
+        int jmllulus = 0;
 
         for (int i = 0; i < nilaiMhs.length; i++){
-            System.out.print("Masukkan nilai Mahasiswa ke-" + i+ ":");
+            System.out.print("Masukkan nilai Mahasiswa ke-" + i+1+ ":");
             nilaiMhs[i] = sc27.nextInt();
         }
-        for (int i = 0; i < nilaiMhs.length; i++){
-            total = total + nilaiMhs[i];
+        for (int i =0 ; i < nilaiMhs.length; i++){
+            if (nilaiMhs[i] > 70){
+                totalLulus += nilaiMhs[i];
+                jmllulus++;
+            }else{
+                totaltdklulus += nilaiMhs[i];
+            }
         }
-        rata2 = total / nilaiMhs.length;
-        System.out.print("Rata-rata nilai = " + rata2);
-
-
+        ratalulus = totalLulus / jmllulus;
+        ratatdkLulus = totaltdklulus / (nilaiMhs.length - jmllulus);
+        System.out.println("Rata-rata nilai lulus = " + ratalulus);
+        System.out.print("Rata-rata nilai tidak lulus = " + ratatdkLulus);
     }
 }
