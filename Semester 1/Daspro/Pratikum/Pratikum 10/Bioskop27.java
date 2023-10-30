f6f89ce55945d7fee08deef34d35d7d5861016d9
taufikdimas <taufikdimasedystara@gmail.com>
2023-10-30 13:22:01 +0700

    fix :Menampilkan nama penonton dengan foreach loop

--- Semester 1/Daspro/Pratikum/Pratikum 10/Bioskop27.java
+++ Semester 1/Daspro/Pratikum/Pratikum 10/Bioskop27.java
@@ -25,9 +25,10 @@ public class Bioskop27 {
        for (String[] barisPenonton : penonton){
         System.out.println("Panjang Baris : " + barisPenonton.length);
        }
+       //menampilkan nama penonton menggunakan for each loop
        System.out.println("Penonton pada baris ke-3 :");
-       for (int i = 0;i < penonton[2].length;i++){
-        System.out.println(penonton[2][i]);
+       for (String i : penonton[2]){
+        System.out.println(i);
        }
 
     }
