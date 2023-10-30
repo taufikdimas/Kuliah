1c8cc839446786532728df09dc0298b748fd1396
taufikdimas <taufikdimasedystara@gmail.com>
2023-10-30 12:49:01 +0700

    fix : modifikasi menggunakan foreach loop

--- Semester 1/Daspro/Pratikum/Pratikum 10/Bioskop27.java
+++ Semester 1/Daspro/Pratikum/Pratikum 10/Bioskop27.java
@@ -22,9 +22,9 @@ public class Bioskop27 {
 
         System.out.println(penonton.length);
 
-        for (int i = 0; i < penonton.length; i++){
-            System.out.println("Panjang baris ke-" + (i+1) + " : " + penonton[i].length);
-        }
+       for (String[] barisPenonton : penonton){
+        System.out.println("Panjang Baris : " + barisPenonton.length);
+       }
 
     }
 }
