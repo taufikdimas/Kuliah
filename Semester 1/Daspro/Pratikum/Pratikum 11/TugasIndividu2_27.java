ccc8e02ae96ea6d5f2ded7e2561b0abf3193046f
taufikdimas <taufikdimasedystara@gmail.com>
2023-11-06 15:43:02 +0700

    feat : Tugas Individu 2

---
+++ Semester 1/Daspro/Pratikum/Pratikum 11/TugasIndividu2_27.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class TugasIndividu2_27 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Masukkan nilai N (minimal 5): ");
+        int N = input.nextInt();
+
+        for (int i = N; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
