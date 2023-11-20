e4191ba84042feaca54d03f977b40d25a48178c2
taufikdimas <taufikdimasedystara@gmail.com>
2023-11-20 13:49:03 +0700

    feat : Jobsheet 13 percobaan 4

--- Semester 1/Daspro/Pratikum/Pratikum 13/UcapanTerimaKasih_27.java
+++ Semester 1/Daspro/Pratikum/Pratikum 13/UcapanTerimaKasih_27.java
@@ -2,7 +2,11 @@ import java.util.Scanner;
 
 //percobaan 4
 public class UcapanTerimaKasih_27 {
-    public static String PenerimaUcapan() {
+    public static void main(String[] args) {
+        UcapanTerimaKasih();
+    }
+
+    public static String PenerimaUcapan() { // fungsi 1
         Scanner sc27 = new Scanner(System.in);
         System.out.print("Tuliskan NAMA yang ingin Anda beri ucapan :");
         String namaOrang = sc27.nextLine();
@@ -10,13 +14,9 @@ public class UcapanTerimaKasih_27 {
         return namaOrang;
     }
 
-    public static void UcapanTerimaKasih() {
+    public static void UcapanTerimaKasih() {// fungsi2
         String nama = PenerimaUcapan();
         System.out.println("thank you " + nama + " for being the best teacher in the world.\n" +
                 "you inspired in me a love for learning and made me feel like i could ask you anything.");
-    }
-
-    public static void main(String[] args) {
-        UcapanTerimaKasih();
     }
 }
