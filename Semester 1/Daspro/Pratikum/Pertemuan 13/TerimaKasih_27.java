bb1f29261221b05fd527e4afcb86d77d3fc53d8e
taufikdimas <taufikdimasedystara@gmail.com>
2023-11-20 12:33:21 +0700

    feat : percobaan 1 dan 2

---
+++ Semester 1/Daspro/Pratikum/Pertemuan 13/TerimaKasih_27.java
@@ -0,0 +1,18 @@
+public class TerimaKasih_27 {
+    public static void UcapanTerimaKasih() {// percobaan1
+        System.out.println("Menggunakan Fungsi\n" + "thank you for being the best teacher in the world.\n" +
+                "you inspired in me a love for learning and made me feel like i could ask you anything.");
+    }
+
+    static void UcapanTambahan(String ucapan) {// percobaan 2
+        System.out.println(ucapan);
+    }
+
+    public static void main(String[] args) {// memanggil fungsi
+        UcapanTerimaKasih();
+        System.out.println("Tanpa Menggunakan Fungsi\n" + "thank you for being the best teacher in the world.\n" +
+                "you inspired in me a love for learning and made me feel like i could ask you anything.");
+        String ucapan = "Terima Kasih Pak... Bu... semoga sehat selalu\n" + "Bismillah A";
+        UcapanTambahan(ucapan);
+    }
+}
