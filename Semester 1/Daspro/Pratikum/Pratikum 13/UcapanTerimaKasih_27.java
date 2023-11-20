f5bd072f04e63034f9b57da373414ea64088fdfb
taufikdimas <taufikdimasedystara@gmail.com>
2023-11-20 14:47:48 +0700

    modify : ucapan tambahan

--- Semester 1/Daspro/Pratikum/Pratikum 13/UcapanTerimaKasih_27.java
+++ Semester 1/Daspro/Pratikum/Pratikum 13/UcapanTerimaKasih_27.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 public class UcapanTerimaKasih_27 {
     public static void main(String[] args) {
         UcapanTerimaKasih();
+        UcapanTambahan(" semoga IPmu 4.0");
     }
 
     public static String PenerimaUcapan() { // fungsi 1
@@ -18,5 +19,9 @@ public class UcapanTerimaKasih_27 {
         String nama = PenerimaUcapan();
         System.out.println("thank you " + nama + " for being the best teacher in the world.\n" +
                 "you inspired in me a love for learning and made me feel like i could ask you anything.");
+    }
+
+    public static void UcapanTambahan(String ucapan) {// fungsi 3
+        System.out.println(ucapan);
     }
 }
