b700be362a8db79e00a71e08a678a63c7583f482
taufikdimas <taufikdimasedystara@gmail.com>
2023-11-20 13:50:32 +0700

    feat : jobsheet 13 Percobaan 3

--- Semester 1/Daspro/Pratikum/Pratikum 13/Ucapan_27.java
+++ Semester 1/Daspro/Pratikum/Pratikum 13/Ucapan_27.java
@@ -1,6 +1,11 @@
 import java.util.Scanner;
 
 public class Ucapan_27 {
+    public static void main(String[] args) {
+        String nama = PenerimaUcapan();
+        System.out.println("Thank you " + nama + "\nMay the force be with you.");
+    }
+
     public static String PenerimaUcapan() {
         Scanner sc27 = new Scanner(System.in);
         System.out.print("Tuliskan NAMA orang yang ingin Anda beri ucapan :");
@@ -9,8 +14,4 @@ public class Ucapan_27 {
         return namaOrang;
     }
 
-    public static void main(String[] args) {
-        String nama = PenerimaUcapan();
-        System.out.println("Thank you " + nama + "\nMay the force be with you.");
-    }
 }
