c4a29b5c111c90f1bc32ff0b40067a4c061fa73f
taufikdimas <taufikdimasedystara@gmail.com>
2023-11-20 13:51:22 +0700

    feat : Jobsheet 13 Percobaan 1 dan 2

--- Semester 1/Daspro/Pratikum/Pratikum 13/TerimaKasih_27.java
+++ Semester 1/Daspro/Pratikum/Pratikum 13/TerimaKasih_27.java
@@ -1,4 +1,12 @@
 public class TerimaKasih_27 {
+    public static void main(String[] args) {// memanggil fungsi
+        UcapanTerimaKasih();
+        System.out.println("Tanpa Menggunakan Fungsi\n" + "thank you for being the best teacher in the world.\n" +
+                "you inspired in me a love for learning and made me feel like i could ask you anything.");
+        String ucapan = "Terima Kasih Pak... Bu... semoga sehat selalu\n" + "Bismillah A";
+        UcapanTambahan(ucapan);
+    }
+
     public static void UcapanTerimaKasih() {// percobaan1
         System.out.println("Menggunakan Fungsi\n" + "thank you for being the best teacher in the world.\n" +
                 "you inspired in me a love for learning and made me feel like i could ask you anything.");
@@ -8,11 +16,4 @@ public class TerimaKasih_27 {
         System.out.println(ucapan);
     }
 
-    public static void main(String[] args) {// memanggil fungsi
-        UcapanTerimaKasih();
-        System.out.println("Tanpa Menggunakan Fungsi\n" + "thank you for being the best teacher in the world.\n" +
-                "you inspired in me a love for learning and made me feel like i could ask you anything.");
-        String ucapan = "Terima Kasih Pak... Bu... semoga sehat selalu\n" + "Bismillah A";
-        UcapanTambahan(ucapan);
-    }
 }
